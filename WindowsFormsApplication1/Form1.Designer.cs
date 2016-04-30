ba6c30774548e2cc0c23c4ee2b76f159b8847607
Marat <marat-ab@mail.ru>
2016-05-01 00:18:05 +0500

    [create repository]

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvTable = new System.Windows.Forms.ListView();
+            this.chAddress = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chValue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tbCell = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lvTable
+            // 
+            this.lvTable.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lvTable.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chAddress,
+            this.chValue});
+            this.lvTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvTable.FullRowSelect = true;
+            this.lvTable.GridLines = true;
+            this.lvTable.Location = new System.Drawing.Point(0, 0);
+            this.lvTable.Name = "lvTable";
+            this.lvTable.Size = new System.Drawing.Size(407, 273);
+            this.lvTable.TabIndex = 0;
+            this.lvTable.UseCompatibleStateImageBehavior = false;
+            this.lvTable.View = System.Windows.Forms.View.Details;
+            this.lvTable.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lvTable_MouseDoubleClick);
+            // 
+            // chAddress
+            // 
+            this.chAddress.Text = "Address";
+            // 
+            // chValue
+            // 
+            this.chValue.Text = "Value";
+            this.chValue.Width = 63;
+            // 
+            // tbCell
+            // 
+            this.tbCell.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbCell.Location = new System.Drawing.Point(276, 28);
+            this.tbCell.Name = "tbCell";
+            this.tbCell.Size = new System.Drawing.Size(100, 13);
+            this.tbCell.TabIndex = 1;
+            this.tbCell.Visible = false;
+            this.tbCell.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbCell_KeyPress);
+            this.tbCell.Leave += new System.EventHandler(this.tbCell_Leave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 273);
+            this.Controls.Add(this.tbCell);
+            this.Controls.Add(this.lvTable);
+            this.Name = "Form1";
+            this.Text = "Simple App: in-place editing  of ListView subitems";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvTable;
+        private System.Windows.Forms.ColumnHeader chAddress;
+        private System.Windows.Forms.ColumnHeader chValue;
+        private System.Windows.Forms.TextBox tbCell;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Table filler
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                ListViewItem lvi = new ListViewItem();
+                lvi.Text = i.ToString();
+                lvi.SubItems.Add(rnd.Next(0, 100).ToString());
+                lvTable.Items.Add(lvi);
+            }
+        }
+        
+        bool CancelEdit = false;
+        ListViewItem.ListViewSubItem CurrentSubItem = default(ListViewItem.ListViewSubItem);
+        ListViewItem CurrentItem = default(ListViewItem);
+
+        private void lvTable_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // Get current item of ListView
+            CurrentItem = lvTable.GetItemAt(e.X, e.Y);
+            if (CurrentItem == null)
+                return;
+
+            // Get sub item of current item
+            CurrentSubItem = CurrentItem.GetSubItemAt(e.X, e.Y);
+
+            int SubItembIndex = CurrentItem.SubItems.IndexOf(CurrentSubItem);
+
+            // Check that we try edit column "Value"
+            switch (SubItembIndex)
+            {
+                case 1:
+                    break;
+                default:
+                    return;
+            }
+            
+            // Set params for TextBox, show it and set focus
+            int lLeft = CurrentSubItem.Bounds.Left + 2;
+            int lWidth = CurrentSubItem.Bounds.Width - 2;
+            tbCell.SetBounds(lLeft + lvTable.Left, CurrentSubItem.Bounds.Top + lvTable.Top, lWidth, CurrentSubItem.Bounds.Height);
+            tbCell.Text = CurrentSubItem.Text;
+            tbCell.Show();
+            tbCell.Focus();
+        }
+
+        private void tbCell_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar)
+            {
+                // If you press Enter than copy data from TextBox to ListView cell
+                case (char)Keys.Return:
+                    CancelEdit = false;
+                    e.Handled = true;
+                    tbCell.Hide();
+                    break;
+
+                // If you press Escape than data in ListView cell stay without changes
+                case (char)Keys.Escape:
+                    CancelEdit = true;
+                    e.Handled = true;
+                    tbCell.Hide();
+                    break;
+            }
+        }
+
+        private void tbCell_Leave(object sender, EventArgs e)
+        {
+            tbCell.Hide();
+
+            if (CancelEdit == false)
+            {               
+                CurrentSubItem.Text = tbCell.Text;
+            }
+            else
+            {
+                CancelEdit = false;
+            }
+
+            lvTable.Focus();
+        }
+    }
+}
+
